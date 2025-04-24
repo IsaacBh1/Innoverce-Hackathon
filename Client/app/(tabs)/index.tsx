7963430e936741e465be71c126475d35591af238
Moahmmed-Islam123 <mi.benyahkem@esi-sba.dz>
2025-04-24 21:45:16 +0100

    RefactorV1

--- Client/app/(tabs)/index.tsx
+++ Client/app/(tabs)/index.tsx
@@ -2,39 +2,31 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import NavigationButton from "../../components/ui/NavigationButton.jsx";
 import Feather from "react-native-vector-icons/Feather.js";
-<<<<<<< HEAD
-import Hommmee from "./Hotels/index.tsx";
-
-=======
-import TransportScreen from "./TransportScreen.jsx";
->>>>>>> f039f04f8a24c9be2b36c6b67ae59a3edf50a48a
+import Hotels from "./Hotels/index.tsx";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { TransportScreen } from "./TransportScreen.jsx";
 const Tab = createBottomTabNavigator();
 
 export default function HomeScreen() {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
       <Tab.Screen
-<<<<<<< HEAD
-        name="Hola"
-        component={Hommmee}
-=======
-        name="Home"
-        children={() => <TransportScreen/>}
->>>>>>> f039f04f8a24c9be2b36c6b67ae59a3edf50a48a
+        name="Hotels"
+        component={Hotels}
         options={{
-          title: "Home",
+          title: "Hotels",
           tabBarIcon: ({ color, size }) => (
-            <Feather name="home" color={color} size={size} />
+            <FontAwesome5 name="hotel" color={color} size={22} />
           ),
         }}
       />
       <Tab.Screen
-        name="Black"
-        children={() => <NavigationButton name="Home" icon="home" />}
+        name="Transport"
+        children={() => <TransportScreen />}
         options={{
-          title: "koukou",
+          title: "Transport",
           tabBarIcon: ({ color, size }) => (
-            <Feather name="home" color={color} size={size} />
+            <FontAwesome5 name="bus-alt" color={color} size={22} />
           ),
         }}
       />
