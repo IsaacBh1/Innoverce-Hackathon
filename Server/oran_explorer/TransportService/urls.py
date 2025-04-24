86f3e29418cec20fc1efb06d12b43d163fc5a2b0
SohaibCdr <163554642+SohaibCdr@users.noreply.github.com>
2025-04-24 23:53:07 +0100

    Adding data to db

--- Server/oran_explorer/TransportService/urls.py
+++ Server/oran_explorer/TransportService/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import NearbyVehiclesView, UpdateVehicleLocationView,TouristGuideListView,TaxiDetailView, TouristGuideDetailView,FavoriteTouristGuideView , TouristFavoritesView
+from .views import NearbyVehiclesView, UpdateVehicleLocationView,CreateTaxiView,CreateTaxiDriverView,TouristGuideCreateView,CreateTouristView,TouristGuideListView,TaxiDetailView, TouristGuideDetailView,FavoriteTouristGuideView , TouristFavoritesView
 
 urlpatterns = [
     path("vehicles/nearby/", NearbyVehiclesView.as_view(), name="nearby-vehicles"),
@@ -9,4 +9,8 @@ urlpatterns = [
     path('favorites/add/', FavoriteTouristGuideView.as_view(), name='add_favorite'),
     path('favorites/<int:tourist_id>/', TouristFavoritesView.as_view(), name='list_favorites'),
     path('taxi/<int:taxi_id>/', TaxiDetailView.as_view(), name='taxi_detail'),
+    path('createTourist/', CreateTouristView.as_view(), name='create-tourist'),
+    path('guides/create/', TouristGuideCreateView.as_view(), name='create-tourist-guide'),
+    path('taxis/create/', CreateTaxiView.as_view(), name='create_taxi'),
+    path('taxi-drivers/create/', CreateTaxiDriverView.as_view(), name='create_taxi_driver'),
 ]
