42f15db7b98e1aef462e1998c935242b6a233702
IsaacBh1 <isaac2548623@gmail.com>
2025-04-24 15:39:07 +0100

    make the nav buttom bar

--- Client/app/(tabs)/index.tsx
+++ Client/app/(tabs)/index.tsx
@@ -1,14 +1,31 @@
-import NavigationButton from "../../components/ui/NavigationButton.jsx";
+import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-export default function HomeScreen() {
-  const Tab = createBottomTabNavigator();
+import NavigationButton from "../../components/ui/NavigationButton.jsx";
+import Feather from "react-native-vector-icons/Feather.js";
 
+const Tab = createBottomTabNavigator();
+
+export default function HomeScreen() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={ButtomNavigation} />
-      <Tab.Screen name="Settings" component={ButtomNavigation} />
-      <Tab.Screen name="Bla" component={ButtomNavigation} />
-      <Tab.Screen name="Koukou" component={ButtomNavigation} />
+      <Tab.Screen
+        name="Home"
+        children={() => <NavigationButton name="Home" icon="home" />}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Black"
+        children={() => <NavigationButton name="Home" icon="home" />}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="home" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
--- Client/components/ui/NavigationButton.jsx
+++ Client/components/ui/NavigationButton.jsx
@@ -1,14 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
-const NavigationButton = (name , icon) => {
+import { View, Text } from "react-native";
+import React from "react";
+import Feather from "react-native-vector-icons/Feather";
+const NavigationButton = ({name, icon}) => {
   return (
-    <View>
-        
-        <Text>{name}</Text>
-
+    <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
+      {" "}
+      <Feather name={icon} size={22} color="#999" />
+      <Text>{name}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default NavigationButton
+export default NavigationButton;
